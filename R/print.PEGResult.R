85001e607f1cdaa640bb7d08dbcf88052e14126e
mslegrand <legrand@coanalytics.net>
2014-01-07 22:40:17 -0500

    added qp tool

--- R/print.PEGResult.R
+++ R/print.PEGResult.R
@@ -13,7 +13,12 @@
 #' @export
 print.PEGResult<-function(res){
   if(!("PEGResult" %in% class(res))){ stop("Argument not a peg parsing result")} 
-  cat(paste0("Call: Rule=", paste0(  res$Call$rule.id, "; Input Arg=\"",res$Call$arg,"\" ; Options: debugTree=",res$options$debugTree,",exe=",res$options$exe,")\n")  ))
+  odt<-res$options$debugTree
+  oex<-res$options$exe
+  tmp<-c("; Options: ",": debugTree=", res$options$debugTree, ": exe=", res$options$exe)
+  tmq<-c(oex| odt, odt, odt, oex, oex)
+  opts=ifelse( oex | odt, tmp[tmq], "")
+  cat(paste0("Call: Rule=", paste0(  res$Call$rule.id, "; Input Arg=\"",res$Call$arg,"\" ",opts, "\n")  ))
   #cat(paste0("Call: (", paste0( res$Call$parserName,",", res$Call$rule.id, ",",res$Call$arg,",debugTree=",res$options$debugTree,",exe=",res$options$exe,")\n")  ))
   #cat(paste("Options:", "Apply Actions=",res$options$exe,"Make Tree=",res$options$debugTree,"\n"))
   cat(paste("Status:", res$ok, "\n") ) 
---
+++ R/qp.R
@@ -0,0 +1,31 @@
+#' A simple tool to examine a parsing expression in isolation
+#' 
+#' This can be quite useful in getting to know the PEG syntax
+#' @parameter expression, a parsing expression (the right hand side of a rule definiton) 
+#' @param arg, text string to apply expression to.
+#' @param debugTree=TRUE, By default TRUE, so one can immediately graph (plot) or print (tree)
+#' @return PEGResult, an object giving the result
+#' @examples
+#' # A simple choice operator
+#' qp("'a' / 'b'", "ab")
+#' # A simple sequence operator
+#' qp("'a' 'b'", "ab")
+#' # A lookahead not operator
+#' qp("'a' !'b'", "ab" )
+#' qp("'a' !'b'", "ac" )
+#' # An lookahead and operator
+#' qp("'a' & 'b'", "ab")
+#' qp("'a' & 'b'", "ac")
+#' # An optional operator
+#' qp("'a' 'b'?", "ab")
+#' qp("'a' 'b'?", "ac")
+#' qp("'a'? 'b'", "ab")
+#' qp("'a'? 'b'", "ba")
+#' @export
+qp<-function(expression, arg, debugTree=TRUE){
+  peg<-new.parser()
+  add_rule(peg, paste("R<-",expression))->res
+  apply_rule(peg, 'R', input)->res
+  res$Call$rule.id<-"Anonomous"
+  return(res)
+}
