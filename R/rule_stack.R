2388622588d42fb11c9dfc0afa47334fb1e23ac1
mslegrand <legrand@coanalytics.net>
2014-01-23 15:43:32 -0500

    added dontrun to examples of rule_stack

--- R/rule_stack.R
+++ R/rule_stack.R
@@ -8,10 +8,12 @@
 #' and hence can detect possible infinite recursive calls. To inspect the calling sequence of those
 #' rules when the max depth of the rule stack is exceed, use \code{\link{get_rule_stack}}
 #' @examples
+#' \dontrun{
 #' peg<-new.parser()
 #' set_rule_stack_limit(peg, 10)
 #' add_rule(peg, "A<-A" ) #an infinite recursive call
 #' apply_rule(peg, "A", "x") #the input is irrevelant, throws error
+#' }
 #' @seealso \code{\link{get_rule_stack}}, \code{\link{unset_rule_stack_limit}}
 #' @export
 set_rule_stack_limit<-function(pegR, stop.level.limit){
@@ -25,6 +27,7 @@ set_rule_stack_limit<-function(pegR, stop.level.limit){
 #' @param parser, a peg parser produced by  new.parser
 #' 
 #' @examples
+#' \dontrun{
 #' peg<-new.parser()
 #' set_rule_stack_limit(peg, 10)
 #' add_rule(peg, "A<-'a' A / ''" ) #consumes all a's at the beginning of a string
@@ -32,6 +35,7 @@ set_rule_stack_limit<-function(pegR, stop.level.limit){
 #' #error, rule stack depth limit exceeded
 #' unset_rule_stack_limit(peg)
 #'apply_rule(peg, "A", "aaaaaaaaaaaax") #now succeeds
+#'}
 #' @seealso \code{\link{get_rule_stack}}, \code{\link{set_rule_stack_limit}}
 #' @export
 unset_rule_stack_limit<-function(pegR){
@@ -46,6 +50,7 @@ unset_rule_stack_limit<-function(pegR){
 #' prior to the exceeding the stop level
 #' 
 #' @examples
+#' \dontrun{
 #' peg<-new.parser()
 #' set_rule_stack_limit(peg, 20)
 #' add_rule(peg, "A<-B" ) 
@@ -53,6 +58,7 @@ unset_rule_stack_limit<-function(pegR){
 #' add_rule(peg, "C<-A") # A vicious circle of calls resulting in infinite recursion
 #' apply_rule(peg, "A", "x") # will stop at 20
 #' get_rule_stack(peg)
+#' }
 #' @export
 get_rule_stack<-function(pegR){
   pexGetStack(pegR)
